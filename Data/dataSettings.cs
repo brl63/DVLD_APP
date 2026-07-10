308254ba19b650eecac98ba9c95f32566a07bb6f
brl63 <xdmedo63@gmail.com>
2026-07-10 11:20:31 -0700

    Add project files.

---
+++ Data/dataSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class dataSettings
+    {
+        public static string connectionString = "Data Source=.;Initial Catalog=DVLD;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
