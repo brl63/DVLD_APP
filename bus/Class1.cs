2e2e9776695a2d0757cd2de419e416b73b1aa66d
brl63 <xdmedo63@gmail.com>
2026-07-14 11:16:47 -0700

    feat : added the classe international license in data layer

--- bus/Class1.cs
+++ bus/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Data;
@@ -12,3 +13,4 @@ namespace bus
         public enum enApplicationStatus { New = 1, Cancelled = 2, Completed = 3 }
     }
 }
+
